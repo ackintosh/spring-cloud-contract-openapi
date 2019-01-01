568bf6d2eabdeb3aa4fc08d0503be1aeeaf1efbd
akihito.nakano <sora.akatsuki@gmail.com>
2019-01-01 23:33:11 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/ackintosh/springframework/cloud/contract/verifier/spec/openapi/OpenAPIContractConverter.kt
@@ -0,0 +1,6 @@
+package com.github.ackintosh.springframework.cloud.contract.verifier.spec.openapi
+
+import io.swagger.v3.oas.models.PathItem
+import org.springframework.cloud.contract.spec.ContractConverter
+
+class OpenAPIContractConverter: ContractConverter<Collection<PathItem>>
